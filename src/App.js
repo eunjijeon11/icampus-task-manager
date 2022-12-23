7a46c44bc659c6f9c2167673c403b4b47a715bec
eunzee <eunjiamy@naver.com>
2022-12-24 02:26:03 +0900

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { ChakraProvider, Button, ButtonGroup } from "@chakra-ui/react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <ChakraProvider>
+            <div className="App">
+                <Button colorScheme="green">Button</Button>
+            </div>
+        </ChakraProvider>
+    );
 }
 
 export default App;
